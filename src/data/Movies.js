df2e49f5b80831f0e37174678de6e76a741cf4ad
deshmukh-faiz <126980027+deshmukh-faiz@users.noreply.github.com>
2023-11-16 23:27:11 +1300

    Delete src/data/Movies.js

--- src/data/Movies.js
+++
@@ -1,9 +0,0 @@
-// class Movies{
-//   api() {
-//   return fetch("https://reactnative.dev/movies.json")
-//   .then((response)=>{
-//   return response.json();
-//   })
-//   }
-// }
-//   export default Movies;
